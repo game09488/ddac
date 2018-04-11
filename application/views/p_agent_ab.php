8ce3d9a3cdc742f39502775b3801c6ad84ac544a
game09488 <38254516+game09488@users.noreply.github.com>
2018-04-11 14:48:28 +0800

    Update p_agent_ab.php

--- application/views/p_agent_ab.php
+++ application/views/p_agent_ab.php
@@ -1,5 +1,5 @@
  <?php  
- $connect = mysqli_connect("localhost", "root", "", "db_admin");  
+ $connect = mysqli_connect("ddac2018.mysql.database.azure.com", "game09488@ddac2018", "Knd89757879", "db_admin");  
  $sql = "SELECT * FROM booking INNER JOIN agent WHERE booking.admin_agent_id = agent.agent_id";
  $result = mysqli_query($connect, $sql);
  ?>
