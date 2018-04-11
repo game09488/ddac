ae8d0eed52109e83bdc953147a5463f56ed5dccf
game09488 <38254516+game09488@users.noreply.github.com>
2018-04-11 14:37:39 +0800

    Update p_admin.php

--- application/views/p_admin.php
+++ application/views/p_admin.php
@@ -1,5 +1,5 @@
  <?php  
- $connect = mysqli_connect("localhost", "root", "", "db_admin");  
+ $connect = mysqli_connect("ddac2018.mysql.database.azure.com", "game09488@ddac2018", "Knd89757879", "db_admin");  
  $sql = "SELECT booking_id,status FROM booking WHERE status='Pending'";
  $result = mysqli_query($connect, $sql);
  ?>
